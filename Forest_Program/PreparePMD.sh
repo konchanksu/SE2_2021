02d0261964c9091d1fc9c20043c1bacd6889e4b4
NekoZ-Meow <okc5424938d474w@gmail.com>
2021-06-24 15:39:10 +0900

    Makefile, build.xmlの編集

--- Forest_Program/PreparePMD.sh
+++ Forest_Program/PreparePMD.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 PMD="pmd-bin"
+PMDDIR="pmd"
 PMDVersion="${PMD}-6.35.0"
-PMDArchive="./PMDArchives/${PMDVersion}.zip"
-PMDDirectory="./${PMDVersion}/"
+PMDArchive="./${PMDDIR}/PMDArchives/${PMDVersion}.zip"
+PMDDirectory="./${PMDDIR}/${PMDVersion}/"
 
-if [ -e ${PMDArchive} ]
-then
-	if [ ! -e ${PMDDirectory} ]
-	then
+if [ -e ${PMDArchive} ]; then
+	if [ ! -e ${PMDDirectory} ]; then
 		unzip ${PMDArchive}
 		ln -s ${PMDDirectory} ${PMD}
 	else
-		if [ ! -d ${PMDDirectory} ]
-		then
+		if [ ! -d ${PMDDirectory} ]; then
 			rm -rf ${PMDDirectory}
 			unzip ${PMDArchive}
 			ln -s ${PMDDirectory} ${PMD}
