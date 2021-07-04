543366386bd8bde53b1f80cb4765da8743cbf7c5
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2021-07-04 15:42:19 +0900

    XXXData系にjava docを追加

--- Forest_Program/forest/data/ForestData.java
+++ Forest_Program/forest/data/ForestData.java
@@ -2,21 +2,43 @@ package forest.data;
 
 import java.util.List;
 
+/**
+ * forestに必要なデータを格納するクラス
+ */
 public class ForestData {
 
+	/**
+	 * ノードのリスト
+	 */
 	private List<NodeData> nodeList;
 
+	/**
+	 * ブランチのリスト
+	 */
 	private List<BranchData> branchList;
 
+	/**
+	 * NodeList、BranchListを格納する
+	 * @param aNodeList
+	 * @param aBranchList
+	 */
 	public ForestData(List<NodeData> aNodeList, List<BranchData> aBranchList) {
 		this.nodeList = aNodeList;
 		this.branchList = aBranchList;
 	}
 
+	/**
+	 * NodeListを取得する
+	 * @return NodeList
+	 */
 	public List<NodeData> getNodeList() {
 		return this.nodeList;
 	}
 
+	/**
+	 * BranchListを格納する
+	 * @return BranchList
+	 */
 	public List<BranchData> getBranchList() {
 		return this.branchList;
 	}
