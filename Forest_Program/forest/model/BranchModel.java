b19ddb2a3e52a03001230d9a779af058dcd66f8a
NekoZ-Meow <okc5424938d474w@gmail.com>
2021-07-04 15:45:19 +0900

    BranchModel作成

--- Forest_Program/forest/model/BranchModel.java
+++ Forest_Program/forest/model/BranchModel.java
@@ -1,31 +1,93 @@
 package forest.model;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
+/**
+ * 一つの枝を表すクラス
+ * 
+ * @author Kodai Okayama
+ */
 public class BranchModel {
 
+	/**
+	 * この枝の始点を表すノード
+	 */
 	private NodeModel startNode;
 
+	/**
+	 * この枝の終点を表すノード
+	 */
 	private NodeModel endNode;
 
+	/**
+	 * コンストラクタ。始点と終点を受け取り束縛する
+	 * 
+	 * @param from
+	 * @param to
+	 */
 	public BranchModel(NodeModel from, NodeModel to) {
-
+		this.startNode = from;
+		this.endNode = to;
 	}
 
-	public void changed() {
-
-	}
-
+	/**
+	 * ブランチを表す線を描画する
+	 * 
+	 * @param aGraphics 描画するグラフィックス
+	 */
 	public void draw(Graphics aGraphics) {
 
+		// 始点座標を計算
+		Point position = this.startNode.getPosition();
+		Point extent = this.startNode.getExtent();
+		Integer startX = position.x + extent.x;
+		Integer startY = position.y + (extent.y / 2);
+
+		// 終点座標を計算
+		position = this.endNode.getPosition();
+		extent = this.endNode.getExtent();
+		Integer endX = position.x;
+		Integer endY = position.y + (extent.y / 2);
+
+		// TODO : Constantクラスで色を設定する
+		aGraphics.setColor(Color.black);
+		aGraphics.drawLine(startX, startY, endX, endY);
+
+		return;
 	}
 
+	/**
+	 * この枝の始点を表すノードを返す
+	 * 
+	 * @return 始点を表すノード
+	 */
 	public NodeModel getStart() {
-		return null;
+		return this.startNode;
 	}
 
+	/**
+	 * この枝の終点を表すノードを返す
+	 * 
+	 * @return 終点を表すノード
+	 */
 	public NodeModel getEnd() {
-		return null;
+		return this.endNode;
+	}
+
+	/**
+	 * この枝を文字列に変換する
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("startNode : (");
+		builder.append(this.startNode.toString());
+		builder.append("), endNode : (");
+		builder.append(this.endNode.toString());
+		builder.append(")");
+		return builder.toString();
 	}
 
 }
--- Forest_Program/forest/model/NodeModel.java
+++ Forest_Program/forest/model/NodeModel.java
@@ -1,44 +1,89 @@
 package forest.model;
 
-import java.util.List;
-import java.awt.Point;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.util.List;
 
+/**
+ * 一つのノードを表すクラス
+ * 
+ * @author Kodai Okayama
+ */
 public class NodeModel {
 
+	/**
+	 * このノードの子要素を束縛するリスト
+	 */
 	private List<NodeModel> children;
 
 	/**
-	 * ノードの幅と高さを記録する
+	 * ノードの幅と高さを束縛する
 	 */
 	private Point extent;
 
+	/**
+	 * 一度の描画ですでに描画済みか
+	 */
 	private Boolean isVisited;
 
+	/**
+	 * このノードの名前を束縛する
+	 */
 	private String name;
 
+	/**
+	 * このノードの親要素を束縛する
+	 */
 	private List<NodeModel> parent;
 
+	/**
+	 * このノードの位置を束縛する。座標は左上が原点
+	 */
 	private Point position;
 
+	/**
+	 * コンストラクタ。 座標は(0,0)に設定され、幅と高さは文字の長さによって決定される。
+	 * 
+	 * @param name ノード名
+	 */
 	public NodeModel(String name) {
-
+		this.setName(name);
+		this.setPosition(new Point(0, 0));
+		this.isVisited = false;
 	}
 
-	public void changed() {
-
-	}
-
+	/**
+	 * このノードを描画する
+	 * 
+	 * @param aGraphics 描画するグラフィクス
+	 */
 	public void draw(Graphics aGraphics) {
 
 	}
 
+	/**
+	 * このノードの名前を返す
+	 * 
+	 * @return このノードの名前
+	 */
 	public String getName() {
-		return null;
+		return this.name;
 	}
 
 	public void setName(String name) {
+		this.name = name;
+		// Integer width = this.stringWidth(this.name) + (Constant.Margin.x * 2);
+		// Integer height = this.stringHeight(this.name) + (Constant.Margin.y * 2);
+		// this.setExtent(new Point(width, height));
+	}
 
+	public Point getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(Point aPoint) {
+		this.position = aPoint;
+		return;
 	}
 
 	public Point getExtent() {
