864be7a246ef45077343b700bd086bd00a071132
SugihaM <shanqiaozhenhui@gmail.com>
2021-07-06 18:57:14 +0900

    Constantクラスに定数追加

--- Forest_Program/forest/Constant.java
+++ Forest_Program/forest/Constant.java
@@ -1,15 +1,59 @@
 package forest;
 
 public class Constant extends Object {
-
+	/**
+	 * 初期のウィンドウサイズの幅を示す定数
+	 */
 	public static final Integer WINDOW_WIDTH = 800;
 
+	/**
+	 * 初期のウィンドウサイズの高さを示す定数
+	 */
 	public static final Integer WINDOW_HEIGHT = 700;
 
+	/**
+	 * ウィンドウサイズの幅が縮小できる最小値を示す定数
+	 */
+	public static final Integer WINDOW_WIDTH_MINIMUM = 200;
+
+	/**
+	 * ウィンドウサイズの高さが縮小できる最小値を示す定数
+	 */
+	public static final Integer WINDOW_HEIGHT_MINIMUM = 200;
+
+	/**
+	 * ノードなどが縦に移動する量を示す定数
+	 */
 	public static final Integer VERTICAL_MOVE = 2;
 
+	/**
+	 * ノードなどが横に移動する量を示す定数
+	 */
 	public static final Integer HORIZONTAL_MOVE = 25;
 
+	/**
+	 * アニメーションの時間間隔を示す定数
+	 */
 	public static final Integer SLEEP_TIME = 50;
+	
+	/**
+	 * フォントの詳細を示す定数
+	 */
+	public static final Font DEFAULT_FONT = new Font("Serif",Font.PLAIN,12);
+
+	/**
+	 * ノード文字色を示す定数
+	 */
+	public static final Color NODE_COLOR = Color.black;
+
+	/**
+	 * ブランチの色を示す定数
+	 */
+	public static final Color BRANCH_COLOR = Color.white;
+
+	/**
+	 * 背景の色を示す定数
+	 */
+	public static final Color BACKGROUND_COLOR = Color.white;
 
 }
