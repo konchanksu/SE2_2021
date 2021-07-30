af59e46906a9c0ae350084365163351f32709b06
konchanksu <nyankomi59@gmail.com>
2021-07-31 02:09:53 +0900

    SampleTestのjavadocの改良(これは必要だったのか？)

--- Forest_Program/forest/tests/SampleTest.java
+++ Forest_Program/forest/tests/SampleTest.java
@@ -4,7 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * 仮のテスト
+ * 動くか試すだけ
+ */
 class SampleTest {
+    /**
+     * とりあえずサンプルで実行してみるテスト
+     */
     @Test
     public void testSuccess() {
         assertEquals(true, true);
